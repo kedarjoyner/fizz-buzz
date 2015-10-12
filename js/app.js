ce3f2d3ba011887a9e420dc33a67ffdceef2d6d8
kedarjoyner <kedarjoyner@gmail.com>
2015-10-12 17:00:24 -0500

    gh-pages

---
+++ js/app.js
@@ -0,0 +1,32 @@
+var results = document.getElementById("results");
+
+var listItems = "";
+
+var i;
+ 
+  for (i = 1; i <= 50; i++) {
+    listItems += "<ul>";
+
+    if (i % 3 === 0 && i % 5 === 0) {
+      
+      listItems += "Fizz-Buzz!";
+    }
+    else if (i % 3 === 0) {
+      
+      listItems += "fizz";
+    }
+    else if (i % 5 === 0) {
+      listItems += "buzz";
+    }
+    else {
+    	listItems += i; 
+    }
+
+    listItems += "</ul>"
+}
+
+results.innerHTML += listItems;
+
+
+
+
